7ad7fb5ae872e0d5cc0d713e9c725137b06837fe
Mour Zhao <mour@i.local>
2018-08-19 22:57:14 +0800

    Add load file as method

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
-from models import Model, load_classifications
+from models import Model, load_classifications, dumpit
 import importlib
-import sys
+import os,sys
+import time
 import click
 
 
@@ -24,7 +25,7 @@ def cluster(method,pipe,cross_validation):
     click.echo(click.style("[X] Temporaly Not Implement", fg='red', bg='black'))
 
 @cli.command()
-@click.option('--method',default='allmethod',help="Your method for training model", multiple=True)
+@click.option('--method',default='allmethod',help="Your method for training model")
 @click.option('--pipe',default=None,help="Data Pipe Line File ")
 @click.option('--loss',default='neg_log_loss',help="Model Evaluation Method ")
 @click.option('--cross-validation',default=10, help="Cross Validation ")
@@ -45,17 +46,46 @@ def classification(method,pipe,cross_validation,loss):
                 x_train, y_train, x_test, y_test = datapipe.itrain_pipe()
             else:
                 click.echo(click.style("[X] Data load script was not expected, Please Check it Again",fg='red'))
+  
+    if os.path.isfile("".join(method)):
+        """
+            load train method from file, also need in debug file, so it's etl util ??
+        """
+        from sklearn.model_selection import cross_validate
+        from sklearn import metrics
+        try:
+            loader = importlib.machinery.SourceFileLoader('Model', method)
+            m = loader.load_module()
+            clf = m.Model()
+            clf.fit(x_train, y_train)
+            modeldumpname = "saved/{}-{}.pkl".format(os.path.basename(method), time.ctime().replace(" ","-"))
+            dumpit(clf, modeldumpname)
+            
+            print("Training Score: \t",clf.score(x_test, y_test), end="")
+            if loss in metrics.__all__:
+                loss = getattr(metrics, loss)
+            else:
+                loss = metrics.log_loss
+            y_prob = clf.predict_proba(x_test)
+            y_predict = clf.predict(x_test)
+            
+            print("Classification Report: ", metrics.classification_report(y_test,y_predict))
+            print("{}:{}".format(loss.__name__,loss(y_test, y_prob)))
+        
+        except Exception as e:
+            print(" Exception: {}".format(e))
 
-    methods = [CLASSFICATIONS.get(x) for x in method if CLASSFICATIONS.get(x)]
-    if methods:
-        clfs = Model(methods,loss=loss)
-        clfs.fit(x_train,y_train=y_train,x_test=x_test, y_test=y_test)
-        clfs.save()
     else:
-        click.echo(click.style("[!] Now We Will Use Default All Method", fg="green",bg="black"))
-        clfs = Model(CLASSFICATIONS.values(),loss=loss)
-        clfs.fit(x_train,y_train=y_train,x_test=x_test, y_test=y_test)
-        clfs.save()
+        methods = [CLASSFICATIONS.get(x) for x in method if CLASSFICATIONS.get(x)]
+        if methods:
+            clfs = Model(methods,loss=loss)
+            clfs.fit(x_train,y_train=y_train,x_test=x_test, y_test=y_test)
+            clfs.save()
+        else:
+            click.echo(click.style("[!] Now We Will Use Default All Method", fg="green",bg="black"))
+            clfs = Model(CLASSFICATIONS.values(),loss=loss)
+            clfs.fit(x_train,y_train=y_train,x_test=x_test, y_test=y_test)
+            clfs.save()
 
 def main():
     cli()
