7c632c98c5d7e2ab00d9c2e25331a5961b1eeb43
Mour Zhao <mour@i.local>
2018-08-19 22:56:37 +0800

    add common function to read multiple file

--- pipe/etl/util.py
+++ pipe/etl/util.py
@@ -1,5 +1,7 @@
-
+import os
 import numpy as np
+
+from concurrent.futures import ProcessPoolExecutor
 from sklearn.externals import joblib
 
 def loadmodelfeatures(model_path):
@@ -15,3 +17,21 @@ def loadmodelfeatures(model_path):
     s_weight_columns = [ "f{}".format(i+1) for i in np.where(a_weight!=0.0)[0]]
 
     return s_weight, s_weight_columns, z_weight, zero_weights_columns
+
+def multicall(p,l):
+    """
+        return res with mutliprocessed of list
+        example:
+            multi process read files of folder
+    """
+    res = None
+    with ProcessPoolExecutor() as pool:
+        res = list(pool.map(p,l))
+    return res
+
+def walkfolder(folder):
+    files = []
+    for r,d,f in os.walk(folder):
+        for item in f:
+            files.append(os.path.join(r,item))
+    return files
